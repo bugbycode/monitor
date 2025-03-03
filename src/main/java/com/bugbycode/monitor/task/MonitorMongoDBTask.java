0ab773ad0fa56402b7cf1a45afdd48002470545f
bugbycode <bugbycode@gmail.com>
2025-03-03 14:06:34 +0800

    新增MongoDB进程监控任务

---
+++ src/main/java/com/bugbycode/monitor/task/MonitorMongoDBTask.java
@@ -0,0 +1,35 @@
+package com.bugbycode.monitor.task;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.bugbycode.module.ResultCode;
+import com.util.ProcessUtil;
+
+/**
+ * 进程监控任务
+ */
+@Configuration
+@EnableScheduling
+public class MonitorMongoDBTask {
+
+	private final Logger logger = LogManager.getLogger(MonitorMongoDBTask.class);
+	
+	/**
+	 * 进程监控任务 每1分钟执行一次
+	 * 
+	 * @throws Exception
+	 */
+	@Scheduled(cron = "25 0/1 * * * ?")
+	public void executeShapeTask() {
+
+		ResultCode code = ProcessUtil.startupMongod();
+		if(code == ResultCode.ERROR) {
+			logger.info("Start mongodb error.");
+		}
+		
+	}
+}
